13632f147a6e42b960be0e6951f9a7c58ca5b3a6
Everspace <7560635+Everspace@users.noreply.github.com>
2021-08-08 13:22:36 -0700

    Add state tracking on buttons

--- src/augmenting/AugmentCategoryDisplay.tsx
+++ src/augmenting/AugmentCategoryDisplay.tsx
@@ -1,15 +1,35 @@
+import { groupBy } from "lodash"
 import { useState } from "react"
 import { Button, Container, Icon, Input, Item, Tab } from "semantic-ui-react"
-import { AugmentDisplay } from "./AugmentDisplay"
-import { allAugmentCategories, allAugments } from "./data/augment"
+import { SingleAugmentDisplay } from "./SingleAugmentDisplay"
+import {
+  allAugmentCategories,
+  Augment,
+  augmentByCategory,
+  AugmentCategory,
+} from "./data/augment"
 
 type Pane = {
   menuItem?: any
   render?: (() => React.ReactNode) | undefined
 }
 
-const CategoryPane = ({ category }: { category: string }) => {
+const CategoryPane = ({ category }: { category: AugmentCategory }) => {
   const [search, setSearch] = useState<null | string>(null)
+  const groups = groupBy(augmentByCategory[category], (a) =>
+    a.baseName ? "base" : "noBase",
+  )
+
+  const base = groups["base"]
+  const noBase: Augment[] | undefined = groups["noBase"]
+
+  const baseGroups = groupBy(base, (a) => a.baseName)
+
+  const filterate = (a: Augment) => {
+    if (!search) return true
+    return a.name.indexOf(search) > -1
+  }
+
   return (
     <Tab.Pane>
       <Container>
@@ -33,15 +53,11 @@ const CategoryPane = ({ category }: { category: string }) => {
       </Container>
       <Container>
         <Item.Group relaxed="very" unstackable>
-          {allAugments
-            .filter((a) => a.category === category)
-            .filter((a) => {
-              if (!search) return true
-              return a.name.indexOf(search) > -1
-            })
-            .map((a) => (
-              <AugmentDisplay key={a.name} augment={a} />
-            ))}
+          {noBase
+            ? noBase
+                .filter(filterate)
+                .map((a) => <SingleAugmentDisplay key={a.name} augment={a} />)
+            : null}
         </Item.Group>
       </Container>
     </Tab.Pane>
