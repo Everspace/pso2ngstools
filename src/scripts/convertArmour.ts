6f80d2b9d0963d313eeb8e17856f935b06c8eb03
Everspace <7560635+Everspace@users.noreply.github.com>
2021-12-22 23:39:02 -0800

    First attempt at importing csv

---
+++ src/scripts/convertArmour.ts
@@ -0,0 +1,50 @@
+import { AugmentStat, Unit } from "augmenting/types"
+
+interface DataSheetRow {
+  Series: string
+  Lv: string
+  Stars: string
+  hp: string
+  pp: string
+  MEL: string
+  RNG: string
+  TEC: string
+  DEF: string
+  DEFMax: string
+  DmgResist: string
+  StatusResist: string
+}
+
+type TranslationTable = Partial<Record<keyof DataSheetRow, keyof AugmentStat>>
+
+const translationTable: TranslationTable = {
+  DmgResist: "damageResist",
+  MEL: "meleePotency",
+  RNG: "rangedPotency",
+  TEC: "techPotency",
+  StatusResist: "statusResist",
+  hp: "hp",
+  pp: "pp",
+}
+
+export function handleArmorRow(row: DataSheetRow): Unit {
+  const { Series, Lv, DEF, DEFMax, Stars, ...stats } = row
+
+  const data: Unit = {
+    name: Series,
+    level: Number(Lv),
+    stars: Number(Stars),
+    defenseBase: Number(DEF),
+    defenseMax: Number(DEFMax),
+    stat: {},
+  }
+
+  const processedStats: AugmentStat = Object.fromEntries(
+    (Object.entries(stats) as Array<[keyof TranslationTable, string]>)
+      .filter(([name, value]) => value !== "")
+      .map(([name, value]) => [translationTable[name], Number(value)]),
+  )
+  data.stat = processedStats
+
+  return data
+}
---
+++ src/scripts/convertWeapon.ts
@@ -0,0 +1,39 @@
+import { Weapon } from "augmenting/types"
+
+interface DataSheetRow {
+  Series: string
+  Lv: string
+  Stars: string
+  Base: string
+  GrindMax: string
+  Element: string
+  VarianceLow: string
+  VarianceHigh: string
+}
+
+export function handleWeaponRow(row: DataSheetRow): Weapon {
+  const {
+    Series,
+    Lv,
+    Stars,
+    Base,
+    GrindMax,
+    Element,
+    VarianceLow,
+    VarianceHigh,
+  } = row
+
+  const data: Weapon = {
+    name: Series,
+    level: Number(Lv),
+    stars: Number(Stars),
+    attackBase: Number(Base),
+    attackMax: Number(GrindMax),
+    varianceLow: Number(VarianceLow),
+    varianceHigh: Number(VarianceHigh),
+  }
+
+  if (Element !== "") data.element = Element
+
+  return data
+}
